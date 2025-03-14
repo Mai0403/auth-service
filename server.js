d75b828f9999e2ccde7264fb9ba4b0f48d9406ad
Sreymai Phou <105780579+Mai0403@users.noreply.github.com>
2025-03-14 16:41:59 +0700

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use("/auth", authRoutes);
+app.use("/", authRoutes);
 
 // app.listen(5001, () => console.log("Auth Service running on port 5001"));
 
